bddafa6d0a64f006cc9726c0a947ad6194f883e7
tgiqfe <tqji31001@gmail.com>
2019-06-02 17:27:19 +0900

    Init

---
+++ CheckDomain/ADDomain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace CheckDomain
+{
+    class ADDomain
+    {
+        private readonly static string[] SYSTEM_ACCOUNT =
+            new string[] { "System", "Local Service", "Network Service", Environment.MachineName + "$" };
+
+        public string DomainName { get; set; }
+
+        public ADDomain()
+        {
+            GetDomainName();
+        }
+
+        public string GetDomainName()
+        {
+            ManagementObject mo = new ManagementClass("Win32_ComputerSystem").
+                GetInstances().
+                OfType<ManagementObject>().
+                FirstOrDefault(x => (bool)x["PartOfDomain"]);
+            this.DomainName = mo != null ? mo["Domain"] as string : null;
+            return DomainName;
+        }
+
+        //  実行中のPCがドメイン参加済みかどうか
+        public bool IsDomainPC()
+        {
+            return !string.IsNullOrEmpty(DomainName);
+        }
+
+        //  実行中のPCがワークグループかどうか
+        //  !IsDomainPCでも良いです。
+        public bool IsWorkgroupPC()
+        {
+            return string.IsNullOrEmpty(DomainName);
+        }
+
+        //  実行中のユーザーがシステムアカウント (SYSTEM, Local Service, Network Service) かどうか
+        public bool IsSytsemAccount()
+        {
+            return SYSTEM_ACCOUNT.Any(x => x.Equals(Environment.UserName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        //  実行中のユーザーがドメインユーザーかどうか
+        public bool IsDomainUser()
+        {
+            return !IsSytsemAccount() && IsDomainPC() && (Environment.UserDomainName != Environment.MachineName);
+        }
+
+        //  実行中のユーザーがローカルユーザーかどうか
+        //  !IsDomainUserでも良いです。
+        public bool IsLocalUser()
+        {
+            return !IsSytsemAccount() && IsWorkgroupPC() && (Environment.UserDomainName == Environment.MachineName);
+        }
+    }
+}
---
+++ CheckDomain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDomain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ADDomain domain = new ADDomain();
+
+            Console.WriteLine("ドメイン参加：{0}", domain.IsDomainPC().ToString());
+            Console.WriteLine("ワークグループ：{0}", domain.IsWorkgroupPC().ToString());
+            Console.WriteLine("システムアカウントで実行：{0}", domain.IsSytsemAccount().ToString());
+            Console.WriteLine("ドメインユーザーで実行：{0}", domain.IsDomainUser().ToString());
+            Console.WriteLine("ローカルユーザーで実行：{0}", domain.IsLocalUser().ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
